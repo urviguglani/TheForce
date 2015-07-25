95bba4b9b1ef00beed5f1c34d3e1ae91a71fe5f9
urviguglani <urvi.guglani@gmail.com>
2015-07-26 02:26:27 +0800

    OCR no errors
    image to text done :) :) :)

--- app/src/main/java/com/force/the/smartmail/ImageToText.java
+++ app/src/main/java/com/force/the/smartmail/ImageToText.java
@@ -1,11 +1,11 @@
 package com.force.the.smartmail;
 
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.ImageView;
 
 import java.io.ByteArrayOutputStream;
 
@@ -15,38 +15,36 @@ import java.io.ByteArrayOutputStream;
 
 public class ImageToText {
 
-    private PackageManager imageView;
-    Drawable d;
-    byte[] ba;
+   public static void imageToText(ImageView imageView) {
+       Drawable d = imageView.getDrawable();
 
-    int a = ba.length;
+       Bitmap bitmap = ((BitmapDrawable) d).getBitmap();
+       byte[] ba;
+       do {
+           ByteArrayOutputStream bao = new ByteArrayOutputStream();
 
-    Bitmap bitmap = ((BitmapDrawable) d).getBitmap();
+           Log.e("BEFORE REDUCING",
+                   bitmap.getHeight() + " " + bitmap.getWidth() + " "
+                           + bitmap.getRowBytes() * bitmap.getHeight());
 
-    while ("BYTE LENGTH"+(ba.length/1024))    {
-        ByteArrayOutputStream bao = new ByteArrayOutputStream();
+           bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bao);
 
-        Log.e("BEFORE REDUCING",
-                bitmap.getHeight() + " " + bitmap.getWidth() + " "
-                        + bitmap.getRowBytes() * bitmap.getHeight());
+           ba = bao.toByteArray();
+           if ((ba.length / 1024) >= 650) {
+               bitmap = Bitmap.createScaledBitmap(bitmap,
+                       (int) (bitmap.getWidth() * 0.95),
+                       (int) (bitmap.getHeight() * 0.95), true);
 
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bao);
+           }
 
-        ba = bao.toByteArray();
-        if ((ba.length / 1024) >= 650) {
-            bitmap = Bitmap.createScaledBitmap(bitmap,
-                    (int) (bitmap.getWidth() * 0.95),
-                    (int) (bitmap.getHeight() * 0.95), true);
+           Log.e("BYTE LENGTH", "" + ba.length / 1024);
 
-        }
+       } while ((ba.length / 1024) >= 650);
 
-        Log.e();
+       String ba1 = Base64.encodeToString(ba, Base64.DEFAULT);
+
 
     }
 
-    String ba1 = Base64.encodeToString(ba, Base64.DEFAULT);
 
-    public ImageToText() {
-        d = imageView.getDrawable();
-    }
 }
